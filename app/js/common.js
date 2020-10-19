dc981452753ed033d9d666eca80d06ee47f66a16
unknown <mashagi@yandex.ru>
2020-10-19 16:42:27 +0300

    collapse is ready. Transition doedsn	 work

--- app/js/common.js
+++ app/js/common.js
@@ -7,21 +7,23 @@ $(function() {
     loop: true,
     smartSpeed: 700,
     items: 1,
-    // navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
-    // responsiveClass: true,
-    // responsive: {},
   });
   $('.popnow__carousel').owlCarousel({
     nav: true,
     dots: false,
-    // loop: true,
     smartSpeed: 700,
     items: 5,
   });
 
   $('.goods__button--howdeliver').on('click', function() {
     $('.goods__coll--wrap').toggleClass('collapse');
-	});
 
-
+    if(this.innerHTML == "Узнать больше") 
+    {
+      this.innerHTML = "Свернуть";
+      return;
+    }
+    else this.innerHTML = "Узнать больше";
+  });
+  
 });
