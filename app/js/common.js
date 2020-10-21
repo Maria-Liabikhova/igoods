a58270413e78976026b7a8deb7803ad075c69269
unknown <mashagi@yandex.ru>
2020-10-21 18:45:46 +0300

    media: sections goods--blue and popnow  adapted

--- app/js/common.js
+++ app/js/common.js
@@ -12,7 +12,23 @@ $(function() {
     nav: true,
     dots: false,
     smartSpeed: 700,
-    items: 5,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1280: {
+        items: 5
+      },
+    }
   });
 
   $('.goods__button--howdeliver').on('click', function() {
